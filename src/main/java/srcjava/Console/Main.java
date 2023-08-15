ae5abcafcb233e962670bac05c7d6361446826e6
Warsniek <101266254+Warsniek@users.noreply.github.com>
2023-08-15 18:33:26 +0200

    Create Main.java

---
+++ src/main/java/srcjava/Console/Main.java
@@ -0,0 +1,19 @@
+package srcjava/Console
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> playerNames = Arrays.asList("Bob", "Boby", "Alice", "Fred");
+        List<String> playerColors = Arrays.asList("vert", "bleu", "rouge", "jaune");
+        int boardSize = 4;
+
+        Game game = new Game(boardSize, playerNames, playerColors);
+
+        game.play();
+    }
+
+}
+
