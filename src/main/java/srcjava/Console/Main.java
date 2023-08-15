3acee56cebdbaf52acf893e1d4bed5a7279758da
Warsniek <101266254+Warsniek@users.noreply.github.com>
2023-08-15 18:41:01 +0200

    Update Main.java

--- src/main/java/srcjava/Console/Main.java
+++ src/main/java/srcjava/Console/Main.java
@@ -1,19 +1,55 @@
 package srcjava.Console;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<String> playerNames = Arrays.asList("Bob", "Boby", "Alice", "Fred");
-        List<String> playerColors = Arrays.asList("vert", "bleu", "rouge", "jaune");
-        int boardSize = 4;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of players: ");
+        int numberOfPlayers = scanner.nextInt();
+        scanner.nextLine(); 
+
+        List<String> playerNames = new ArrayList<>();
+        List<String> playerColors = new ArrayList<>();
+        int boardSize = numberOfPlayers;
+
+        String[] validColors = {"bleu", "vert", "jaune", "rouge"};
+
+        for (int i = 0; i < numberOfPlayers; i++) {
+            System.out.print("Enter name for player " + (i + 1) + ": ");
+            String playerName = scanner.nextLine();
+            playerNames.add(playerName);
+
+            String playerColor;
+            while (true) {
+                System.out.print("Enter color for player " + (i + 1) + " (bleu/vert/jaune/rouge): ");
+                playerColor = scanner.nextLine();
+                if (isValidColor(playerColor, validColors)) {
+                    break;
+                } else {
+                    System.out.println("Invalid color. Please choose from bleu, vert, jaune, or rouge.");
+                }
+            }
+            playerColors.add(playerColor);
+        }
 
         Game game = new Game(boardSize, playerNames, playerColors);
 
         game.play();
+
+        scanner.close();
     }
 
+    private static boolean isValidColor(String color, String[] validColors) {
+        for (String validColor : validColors) {
+            if (validColor.equals(color.toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
-
